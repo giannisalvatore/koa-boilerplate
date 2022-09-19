4a11819c79876f707c80c657240ebd5dd17a54b0
GianniSalvatore <31819603+giannisalvatore@users.noreply.github.com>
2022-09-19 11:01:55 +0200

    removed fake credentials

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,10 @@ var pool = db_connection;
 function db_connection() {
     const mysql = require("mysql2");
     const config = {
-        host: "ls-7e32551ddc6e9f6236ef44a6ef4923085dba168e.crg1scbusazn.us-east-1.rds.amazonaws.com",
-        user: "dbmasteruser",
-        password: "q3Taf-*7t2`SDXQ<qV?WsF~k4O9cYCy8",
-        database: "dbmaster",
+        host: "",
+        user: "",
+        password: "",
+        database: "",
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
